51ef0106a6b0595768c784a69901c12ec7806acb
ManishKumar180 <manishkumar97430@gmail.com>
2022-07-13 17:30:11 +0530

    changed eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,13 +18,7 @@ module.exports = {
     'next/core-web-vitals',
   ],
   rules: {
-    // 'prettier/prettier': 'error',
-    "prettier/prettier": [
-      "error",
-      {
-        "endOfLine": "auto"
-      },
-    ],
+    'prettier/prettier': 'error',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
